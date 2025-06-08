45c8bbac3c8d981c877e4cc57a5d8eb222feca8a
Pawarit2001 <ppat112233123@mail.com>
2025-06-08 11:51:01 +0700

    V3 แก้ 07 แล้วก็ทดสอบ

--- lecture07.c
+++ lecture07.c
@@ -1,6 +1,20 @@
+// ตัวแปล กับ Format code
 #include <stdio.h>
 
 void main()
 {
-    printf("Welcome to IOT-SAU");
+    // สร้างตัวแปลต้องมีชนิดข้อมูล ชื่อตัวแปล
+    int aa = 10;                //(%d) //ทศนิยมไม่เกิน 6 หลัก
+    float bb = 20.125;          //(%f) //ทศนิยมไม่เกิน 6 หลัก
+    double cc = 30.32153132;    //(%lf) //ทศนิยมเกิน 6 หลัก
+    char dd = '#';              //(%c) //ตัวอักษร Charactor คือ ตัวอักษรตัวเดียว อยู่ใน '?'
+    char ee[] = "Hello World!"; //(%s) //ข้อความ string คือ ตัวอักษรตั้งแต่ 0 ตัวขึ้นไป อยู่ใน "????"
+    long ff = 32152154;         //(%ld) //จำนวนเต็มเกินหลักหมื่น
+
+    printf("AA : %d", aa);
+    printf("\nBB : %f", bb);
+    printf("\nCC : %lf", cc);
+    printf("\nDD : %c", dd);
+    printf("\nEE : %s", ee);
+    printf("\nFF : %ld", ff);
 }
